565a8f4112bf38b499c500be0c8c62edc2c79898
Logan Dulski <lduski.07@gmail.com>
2022-04-13 11:46:24 -0400

    image carosel update

--- app/js/java.js
+++ app/js/java.js
@@ -1,25 +1,17 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+const butons = document.querySelectorAll("[data-carosel-button]")
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.caroselButton === "next" ? 1 : -1
+    const slides = button.closest("[data-carosel]").querySelector("[data-slides]")
+    const activeSlide = slide.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
 
-// Next & Previous Controls
-function plusSlides(n){
-   showSlides(slideIndex+= n);
-}
-
-// Image Controls
-function currentslide(n){
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n){ 
-  let i;
-  let slides = document.getElementsByClassName("slides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length} 
-  for (i = 0; i < slides.length; i++){
-    slides[i].style.display = "none";
-  }
-}
+    slides.children[newIndex].dataset.active = true 
+      delete activeSlide.dataset.active
+  })
+})
 // Hamburger Menu
 function menuOnClick(){
   document.getElementsById("menu-bar").classList.toggle("change");
