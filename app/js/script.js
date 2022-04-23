e6d83b249eb76b797a7f372f31fe80e9632fbd49
Logan Dulski <lduski.07@gmail.com>
2022-04-23 12:09:05 -0400

    formatting updates

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,10 @@
-const butons = document.querySelectorAll("[data-carosel-button]")
+const butons = document.querySelectorAll("[data-carousel-button]")
 
 buttons.forEach(button => {
   button.addEventListener("click", () => {
-    const offset = button.dataset.caroselButton === "next" ? 1 : -1
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
     const slides = button
-    .closest("[data-carosel]")
+    .closest("[data-carousel]")
     .querySelector("[data-slides]")
 
     const activeSlide = slides.querySelector("[data-active]")
